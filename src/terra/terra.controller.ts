a11f72ad2b2128ebb56e650d79f439755f20fa13
David Wong <draflier@gmail.com>
2022-04-12 14:50:54 +0800

    include UST Support

--- src/terra/terra.controller.ts
+++ src/terra/terra.controller.ts
@@ -112,6 +112,48 @@ export class TerraController {
       }
     }
 
+    @Get('/activate-ust-poll')
+    @ApiOperation({ summary: 'Poll Src Wallet', description: 'Poll Source Wallet and when account have luna, all luna with be transferred to destination account with defined gas fee in UST' })
+    @ApiResponse({ status: 403, description: 'Forbidden.' })
+    //@ApiQuery({name: 'br_num'})    
+    @ApiQuery({name: 'gas_ust'})
+    @ApiQuery({name: 'to_address'}) 
+    @ApiQuery({name: 'sleep_milli'}) 
+    async activateUstPoll(@Query('gas_ust') numGasFee : number, @Query('to_address')strToAddr : string,  @Query('sleep_milli')numSleepMilli: number) : Promise<string>{
+      try{        
+        Logger.debug("Activating Polling");
+        if (numSleepMilli == undefined)
+        {
+          numSleepMilli = 1000;
+        }
+        else{
+          Logger.debug("Sleep mili => " + numSleepMilli);
+        }
+        numGasFee = numGasFee * 1000000
+        this.terraService.activateUstPoll(numGasFee, strToAddr, numSleepMilli);
+        return "Polling => " + await this.terraService.getAddress();
+      }
+      catch (err)
+      {
+        Logger.debug(err);
+      }
+    }
+
+    @Get('/deactivate-ust-poll')
+    @ApiOperation({ summary: 'Stop Polling', description: 'Stop Polling' })
+    @ApiResponse({ status: 403, description: 'Forbidden.' })  
+    async deactivateUstPoll( ) : Promise<string>{
+      try{        
+        Logger.debug("Deactivate Polling");
+        this.terraService.deactivateUstPoll();
+        return "Polling Stopped";
+      }
+      catch (err)
+      {
+        Logger.debug(err);
+      }
+    }
+
 
     @Get('/get-address')
     @ApiOperation({ summary: 'Get Address', description: 'Get Address' })
@@ -221,6 +263,34 @@ export class TerraController {
         */
         let strAmt : string = await this.terraService.transferAllLunaFixed(intGas, strToAddr);
         let strRes = strAmt + " of Luna (minus gas) is transferred to " + strToAddr
+        return strRes;
+      }
+      catch (err)
+      {
+        Logger.debug(err);
+      }
+    }
+
+    @Get('/transfer-ust-fixed')
+    @ApiOperation({ summary: 'Transfer all UST Balance with stated gas (fixed)', description: 'Transfer all UST Balance with stated gas fee in UST' })
+    @ApiResponse({ status: 403, description: 'Forbidden.' })
+    @ApiQuery({name: 'gas_ust'})   
+    @ApiQuery({name: 'to_address'}) 
+    async transferAllUST(@Query('gas_ust') intGas :number, @Query('to_address')strToAddr : string ) : Promise<string>{
+      try{        
+        Logger.debug("Transferring all ust to => " + strToAddr);
+        
+        intGas = intGas * 1000000;
+        Logger.debug("intGas in uusd => " + intGas);
+        /*      
+                                   
+        let objWalletInfo : WalletInfoDto= await BesuHelper.createWallet(String(br_num) + " " + DPMSHelper.secretSalt);
+        console.log(objWalletInfo);
+        let objApplDtl : DealerApplDtl = await this.terraService.getApplDtl(objWalletInfo);
+        return objApplDtl
+        */
+        let strAmt : string = await this.terraService.transferAllUstFixed(intGas, strToAddr);
+        let strRes = strAmt + " of UST (minus gas) is transferred to " + strToAddr
         return strRes;
       }
       catch (err)
